b1b9ab13b97eb8bc0dde84ccd92de59fb18d2bfa
Tony Korhonen <tony@Tonys-MacBook-Pro.local>
2017-04-11 20:05:54 -0400

    Added project portfolio

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Portfolio',
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Page has an error'
